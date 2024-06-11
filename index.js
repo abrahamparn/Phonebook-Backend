c630d4e3ecf5bac9b06f35fdc92ea6228d791988
abrahamparn <abrahamnaiborhu@gmail.com>
2024-06-11 14:03:10 +0700

    3.10 Phonebook backend step 10 & adding process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 let persons = [
   {
